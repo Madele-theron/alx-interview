420caed7ad782f419bfd45fa7325224d9e7f7ddd
Madele <madele.theron@gmail.com>
2023-04-22 19:27:06 +0200

    removes spaces at end of line

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,7 +2,7 @@
 """
 Pascals triangle of n method:
 """
-    
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the Pascals triangle of n:
@@ -13,9 +13,9 @@ def pascal_triangle(n):
         triangle = []
         for row in range(n):
             #  (0 + 1), (1+1), (2+1), (3+1), (4+1)
-            # this will determine the number of columns in each row: 
+            # this will determine the number of columns in each row:
             next_row = [1] * (row + 1)
             for col in range(1, row):
-                next_row[col] = triangle[row-1][col-1] + triangle[row-1][col] 
+                next_row[col] = triangle[row-1][col-1] + triangle[row-1][col]
             triangle.append(next_row)
         return triangle
