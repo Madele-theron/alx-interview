a161e23a54448d6f589bef270ed653d978948cf4
Madele <madele.theron@gmail.com>
2023-04-22 19:26:01 +0200

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,21 @@
+#!/usr/bin/python3
+"""
+Pascals triangle of n method:
+"""
+    
 def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascals triangle of n:
+    """
     if n <= 0:
         return []
     else:
-        
-    # return list of list of integers representing the Pascal’s triangle of n:
+        triangle = []
+        for row in range(n):
+            #  (0 + 1), (1+1), (2+1), (3+1), (4+1)
+            # this will determine the number of columns in each row: 
+            next_row = [1] * (row + 1)
+            for col in range(1, row):
+                next_row[col] = triangle[row-1][col-1] + triangle[row-1][col] 
+            triangle.append(next_row)
+        return triangle
