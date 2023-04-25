636df9e3a4b93394cd6d3e06ac0190f4427b1384
Madele <madele.theron@gmail.com>
2023-04-25 18:47:17 +0200

    remove "else"

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,17 +9,16 @@ def pascal_triangle(n):
     Returns a list of lists of integers representing the Pascals triangle
     """
     if n <= 0:
-
         return []
-    else:
-        triangle = []
 
-        for row in range(n):
-            # this will determine the number of columns in each row:
-            next_row = [1] * (row + 1)
-            for col in range(1, row):
-                next_row[col] = triangle[row - 1][col - 1] \
-                + triangle[row - 1][col]
-            triangle.append(next_row)
+    triangle = []
 
-        return triangle
+    for row in range(n):
+        # this will determine the number of columns in each row:
+        next_row = [1] * (row + 1)
+        for col in range(1, row):
+            next_row[col] = triangle[row - 1][col - 1] \
+            + triangle[row - 1][col]
+        triangle.append(next_row)
+
+    return triangle
