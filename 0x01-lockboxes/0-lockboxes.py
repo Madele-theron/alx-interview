16baf2921517bfa06bdef665b7cb244b1e38ad20
Madele <madele.theron@gmail.com>
2023-04-27 20:28:51 +0200

    fixed pep8 & pylint

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
 """A method to determine if all the boxes can be opened"""
 
+
 def canUnlockAll(boxes):
+    """A Function that will return True if all boxes are unlocked"""
     keyList = []
     stillLocked = []
-    boxesUnlocked =[]
-    
+    boxesUnlocked = []
+
     for box in boxes:
         currentPosition = boxes.index(box)
         if currentPosition == 0:
             boxesUnlocked.append(currentPosition)
-            # check if there is key in box - if there is keys, we save the keys in a list.
-            # if there is not, we move on to the next box
+
             if not box:
                 print("box is empty, no keys found")
-            else: #maybe I can add a else if currentPosition !== 0 && currentPosition in keyList:...
+            # maybe I can add a else if currentPosition !== 0
+            # && currentPosition in keyList:...
+            else:
                 keysInBox = []
                 for key in box:
                     print("key:", key)
                     keysInBox.append(key)
                     keyList.append(key)
-                # print all keys in that box
-                print (f"boxes[{currentPosition}] -> {keysInBox}")
+                print(f"boxes[{currentPosition}] -> {keysInBox}")
         else:
-            # check if we have a key that matches the currentPosition.
             if currentPosition in keyList:
                 if currentPosition not in boxesUnlocked:
                     boxesUnlocked.append(currentPosition)
-                    # print(f"we can unlock box[{currentPosition}]")
                 keysInBox = []
                 for key in box:
                     print("key:", key)
                     keysInBox.append(key)
                     keyList.append(key)
-                # print all keys in that box
-                print (f"boxes[{currentPosition}] -> {keysInBox}")
+                print(f"boxes[{currentPosition}] -> {keysInBox}")
             else:
                 print(f"box[{currentPosition}] is locked, we don't have a key")
                 stillLocked.append(currentPosition)
@@ -51,12 +50,12 @@ def canUnlockAll(boxes):
                 for key in boxes[box]:
                     keysInBox.append(key)
                     keyList.append(key)
-                print (f"boxes[{box}] -> {keysInBox}")
+                print(f"boxes[{box}] -> {keysInBox}")
                 print(f"saved keys: {keyList}")
                 if box not in boxesUnlocked:
                     boxesUnlocked.append(box)
                 stillLocked.remove(box)
-                print(f"boxes still locked {stillLocked} after we removed box {box}" )
+                print(f"boxes still locked {stillLocked}, removed box {box}")
                 print(f"boxes unlocked: {sorted(boxesUnlocked)}")
                 print(f"boxes that are still locked: {stillLocked}")
             print("current box:", box)
@@ -64,6 +63,15 @@ def canUnlockAll(boxes):
         print("still locked:", stillLocked)
         if not stillLocked:
             return True
-        else: 
-            print("THESE BOXES ARE STILL LOCKED", stillLocked)
-            return False
+        print("THESE BOXES ARE STILL LOCKED", stillLocked)
+        return False
+
+
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))
+
+boxes = [[], [1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))
