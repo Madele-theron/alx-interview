77b574f6f3d000462f604b18aba8100d85e424c7
Madele <madele.theron@gmail.com>
2023-04-27 22:58:05 +0200

    removed empty list in example

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -53,7 +53,7 @@ def canUnlockAll(boxes):
 boxes = [[1], [2], [3], [4], []]
 print(canUnlockAll(boxes))
 
-boxes = [[], [1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
 print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
