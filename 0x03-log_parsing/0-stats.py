03fe32c4301e781075cd1074237e471b9cb1b0ea
Madele <madele.theron@gmail.com>
2023-05-12 07:56:39 +0200

    task 0 stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
-"""Module - log parsing"""
+"""Module -> log parsing"""
 import sys
 import re
 
 if __name__ == "__main__":
-    count = 0
+
     total_size = 0
-    status_codes = {
-        200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
-    }
+    status_codes = {}
 
+    def print_metrics():
+        """computes & prints metrics"""
+        print(f"File size: {total_size}")
+        for code, count in sorted(status_codes.items()):
+            print("{}: {}".format(code, count))
 
-def print_metrics():
-    """"""
-    '''Prints accumulated statistics.'''
-    print("File size: {}".format(count[0]))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code]:
-            print("{}: {}".format(code, status_codes[code]))
+    try:
+        for line in sys.stdin:
+            # Validate the input format
+            if not re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[[^\]]*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d*$', line):
+                continue
 
+            # Get status codes and file sizes
+            match = re.search(r' (\d{3}) (\d+)$', line)
+            status_code = match.group(1)
+            file_size = int(match.group(2))
 
-# regex pattern to match the input format:
-input_format = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[[^\]]*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d*$'
+            total_size += file_size
 
-count = 1
-try:
-    for line in sys.stdin:
-        if re.match(input_format, line):
-            if count % 10 == 0:
+            # Update status code dict
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+            else:
+                status_codes[status_code] = 1
+
+            # Check if we have processed 10 lines
+            if len(status_codes) % 10 == 0:
+                # Print the metrics
                 print_metrics()
-            count += 1
-            
-except KeyboardInterrupt:
-    print_metrics()
-
+    except KeyboardInterrupt:
+        # Print the metrics
+        print_metrics()
