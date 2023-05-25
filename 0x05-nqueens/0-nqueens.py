31840e1d287e465127491c0426789c69f2483b7b
Madele <madele.theron@gmail.com>
2023-05-25 22:55:16 +0200

    initial commit

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+The N queens puzzle is the challenge of placing N non-attacking queens
+on an N×N chessboard. Write a program that solves the N queens problem.
+"""
+import sys
+
+
+def nqueens(n: int):
+    """
+    Method that uses backtracking
+    Args:
+        n (int): number of non-attacking queens
+    Return:
+        
+    """
