749744779cc2860acee1bbcfe9a4bd9b0db466e7
Madele <madele.theron@gmail.com>
2023-06-04 20:48:35 +0200

    canges

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,31 +2,30 @@
 /* Script that prints all characters of a Star Wars movie */
 
 const request = require('request');
-const urlApi = 'https://swapi-api.hbtn.io/api/films/';
-const movieId = process.argv[2];
-// query API
-request(urlApi + movieId, (error, response, body) => {
-  if (error) throw error;
-  const characters = JSON.parse(body).characters;
-  showNames(characters);
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const filmId = process.argv[2];
+
+// if (filmId === undefined) {
+//   exit();
+// }
+
+// Query the api
+request(url + filmId, (error, response, body) => {
+  if (!error) {
+    const people = JSON.parse(body).characters;
+    printPeople(people, 0);
+  } else {
+    console.log(error);
+  }
 });
-// show results on the console
-const showNames = (names, i = 0) => {
-  if (i === names.length) return;
-  request(names[i], (error, response, body) => {
-    if (error) throw error;
-    console.log(JSON.parse(body).name);
-    showNames(names, i + 1);
-  });
-};
 
 // Print people in order to console
-function printPeople (people, i) {
+const printPeople = (people, i) => {
   if (i === people.length) return;
   request(people[i], (error, reponse, body) => {
     if (!error) {
       console.log(JSON.parse(body).name);
-      printPeople(people, i++);
+      printPeople(people, i + 1);
     } else {
       console.log('error', error);
     }
